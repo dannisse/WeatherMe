8dfef174017e3548f8f03993b1cdf855e979f111
Dannisse Arenas <dannisse.a@gmail.com>
2023-02-27 18:09:07 -0500

    Add files via upload
    WeatherMe Proj files added (finally)

---
+++ WeatherMe/WeatherMe/WeatherMe/WeatherMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMeApp.swift
+//  WeatherMe
+//
+//  Created by Dannisse Arenas on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
